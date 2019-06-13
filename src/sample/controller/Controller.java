d73bc5ea3003138bb2c9ee251eccc069a1a84dac
derysudrajat <dery.sudrajat17@gmail.com>
2019-06-14 01:05:46 +0700

    Resolve Controller

---
+++ src/sample/controller/Controller.java
@@ -0,0 +1,33 @@
+package sample.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable, UiLoaderCallback {
+
+    @FXML
+    private AnchorPane main_frame;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loadUI("/sample/layout/register_activity.fxml", main_frame);
+    }
+
+
+    @Override
+    public void loadUI(String layout, AnchorPane pane) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(layout));
+            pane.getChildren().setAll(root);
+        }catch (IOException ignored){
+
+        }
+    }
+}
