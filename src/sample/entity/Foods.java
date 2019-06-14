6b17e21d095fe3d1cc836aca389ad028ca76f3d8
derysudrajat <dery.sudrajat17@gmail.com>
2019-06-14 15:49:47 +0700

    Add Foods model entity class

---
+++ src/sample/entity/Foods.java
@@ -0,0 +1,47 @@
+package sample.entity;
+
+public class Foods {
+    private String name;
+    private String desc;
+    private int price;
+    private float rate;
+
+    public Foods(String name, String desc, int price, float rate) {
+        this.name = name;
+        this.desc = desc;
+        this.price = price;
+        this.rate = rate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public float getRate() {
+        return rate;
+    }
+
+    public void setRate(float rate) {
+        this.rate = rate;
+    }
+}
