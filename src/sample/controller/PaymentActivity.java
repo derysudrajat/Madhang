c8122ab4b2690752085c18c371bc13844c45cc0d
derysudrajat <dery.sudrajat17@gmail.com>
2019-06-15 18:56:48 +0700

    Add PaymentActivity

--- src/sample/controller/PaymentActivity.java
+++ src/sample/controller/PaymentActivity.java
@@ -1,5 +1,4 @@
 package sample.controller;
-
 import com.jfoenix.controls.JFXButton;
 import javafx.event.Event;
 import javafx.event.EventHandler;
@@ -9,26 +8,46 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import sample.helper.UiLoaderCallback;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class MenuCategoryActivity implements Initializable, UiLoaderCallback, EventHandler {
+public class PaymentActivity implements Initializable, UiLoaderCallback, EventHandler {
     @FXML
     private AnchorPane main_frame;
     @FXML
-    private AnchorPane list_frame;
+    private StackPane main_stack_pane;
     @FXML
     private JFXButton btn_back;
     @FXML
-    private JFXButton btn_cart;
+    private Text customer_name;
+    @FXML
+    private Text total_items;
+    @FXML
+    private Text total_price;
+    @FXML
+    private Text list_items;
+    @FXML
+    private JFXButton btn_make_order;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         btn_back.setOnAction(this::handle);
-        btn_cart.setOnAction(this::handle);
-        loadUI("/sample/layout/menu_activity.fxml", list_frame);
+    }
+
+    @Override
+    public void handle(Event event) {
+        EventTarget et = event.getTarget();
+        if (et.equals(btn_back)) {
+            loadUI("/sample/layout/cart_activity.fxml", main_frame);
+        }
+        if (et.equals(btn_make_order)) {
+
+        }
     }
 
     @Override
@@ -38,14 +57,6 @@ public class MenuCategoryActivity implements Initializable, UiLoaderCallback, Ev
             pane.getChildren().setAll(root);
         }catch (IOException ignored){
 
-        }
-    }
-
-    @Override
-    public void handle(Event event) {
-        EventTarget et = event.getTarget();
-        if (et.equals(btn_back)){
-            loadUI("/sample/layout/table_choose_activity.fxml",main_frame);
         }
     }
 }
