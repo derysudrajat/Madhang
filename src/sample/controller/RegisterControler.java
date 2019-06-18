e9b1d219fcb21b2a9fa78790160c7fe1eb2a2a5a
derysudrajat <dery.sudrajat17@gmail.com>
2019-06-18 18:02:50 +0700

    Add validator for tf_menu

--- src/sample/controller/RegisterControler.java
+++ src/sample/controller/RegisterControler.java
@@ -2,11 +2,14 @@ package sample.controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.validation.RequiredFieldValidator;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import sample.helper.Popup;
 import sample.helper.UiLoaderCallback;
 
 import java.io.IOException;
@@ -23,15 +26,28 @@ public class RegisterControler implements Initializable, UiLoaderCallback {
 
     @FXML
     private JFXTextField tf_nama;
+    RequiredFieldValidator validator = new RequiredFieldValidator();
+
+    @FXML
+    void textValidate(KeyEvent event) {
+        if (tf_nama.getText().equals("")) {
+            validator.setMessage("Name Can't be Empty");
+            tf_nama.getValidators().add(validator);
+            tf_nama.validate();
+        } else {
+            tf_nama.resetValidation();
+        }
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         btn_next.setOnAction(event -> {
-
-            if(tf_nama.getText().toString().trim().isEmpty()){
-                System.out.println("JANGAN KOSONG");
+            if (tf_nama.getText().trim().isEmpty()) {
+                validator.setMessage("Field tidak boleh kosong");
+                tf_nama.getValidators().add(validator);
+                tf_nama.validate();
             }else{
-                System.out.println(tf_nama.getText().toString().trim());
+                tf_nama.resetValidation();
                 loadUI("/sample/layout/table_choose_activity.fxml", main_frame);
             }
 
