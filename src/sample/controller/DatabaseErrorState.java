20f3d5fdbe7191d266acd0e31d33feca890f0086
derysudrajat <dery.sudrajat17@gmail.com>
2019-06-19 18:30:10 +0700

    Add DatabaseErrorState Activity

---
+++ src/sample/controller/DatabaseErrorState.java
@@ -0,0 +1,9 @@
+package sample.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+
+public class DatabaseErrorState {
+    @FXML
+    private JFXButton btn_try_again;
+}
