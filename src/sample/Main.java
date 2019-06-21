1c1dbaeb8aa83864da03f6b99e18f28915041d84
derysudrajat <dery.sudrajat17@gmail.com>
2019-06-21 10:47:50 +0700

    Add icon to app

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,6 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("layout/sample.fxml"));
         primaryStage.setTitle("Madhang");
+        primaryStage.getIcons().add(new Image("/sample/res/icon.png"));
         primaryStage.setScene(new Scene(root, 600, 527));
         primaryStage.show();
     }
